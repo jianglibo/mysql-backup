26b178f43ccac1ed835060fc26919ef37e148696
libo <jianglibo@gmail.com>
2018-05-08 13:55:42 +0800

    introduce jooq to this project

---
+++ src/test/java/com/go2wheel/mysqlbackup/jooq/TestJooqCodegen.java
@@ -0,0 +1,21 @@
+package com.go2wheel.mysqlbackup.jooq;
+
+import java.io.InputStream;
+
+import org.jooq.util.GenerationTool;
+import org.jooq.util.jaxb.Configuration;
+import org.junit.Test;
+
+public class TestJooqCodegen {
+	
+	public static final String JOOQ_CONFIG_FILE = "/jooq-config.xml";
+	
+	@Test
+	public void codegen() throws Exception {
+		InputStream in = TestJooqCodegen.class.getResourceAsStream(JOOQ_CONFIG_FILE);
+		Configuration cfg = GenerationTool.load(in);
+		GenerationTool.generate(cfg);
+		in.close();
+	}
+
+}
