330cf111a1d409c5207af4c72307b3c34e59c19c
libo <jianglibo@gmail.com>
2018-05-24 08:01:24 +0800

    add db migrate profile

--- src/main/resources/db/migration/hsqldb/V1_4__unique_mail_cron.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE reuseable_cron ADD CONSTRAINT unique_expression UNIQUE (expression);
-ALTER TABLE mail_address ADD CONSTRAINT unique_email UNIQUE (email);
--- src/main/resources/db/migration/hsqldb/V1_9__add_index.sql
+++
@@ -1,6 +0,0 @@
-CREATE INDEX bd_created_at_idx ON borg_download(created_at);
-CREATE INDEX md_created_at_idx ON mysql_dump(created_at);
-CREATE INDEX mf_created_at_idx ON mysql_flush(created_at);
-CREATE INDEX df_created_at_idx ON diskfree(created_at);
-CREATE INDEX ut_created_at_idx ON up_time(created_at);
-CREATE INDEX bfs_created_at_idx ON backup_folder_state(created_at);
