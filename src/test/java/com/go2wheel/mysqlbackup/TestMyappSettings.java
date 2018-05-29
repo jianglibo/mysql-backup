8b459b811b01fb81e7c6d8b260e88925c18c36c8
libo <jianglibo@gmail.com>
2018-05-29 21:00:26 +0800

    find a way to define list values in application.properties

--- src/test/java/com/go2wheel/mysqlbackup/TestMyappSettings.java
+++ src/test/java/com/go2wheel/mysqlbackup/TestMyappSettings.java
@@ -5,10 +5,19 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.env.Environment;
 
+@Import(com.go2wheel.mysqlbackup.TestMyappSettings.Tcc.class)
 public class TestMyappSettings  extends SpringBaseFort {
 	
 	@Test
@@ -29,10 +38,62 @@ public class TestMyappSettings  extends SpringBaseFort {
 		assertTrue("should in dev profile.", Arrays.stream(env.getActiveProfiles()).anyMatch(p -> "dev".equals(p)));
 	}
 	
+	@Value("#{tenv.aslist}")
+	private List<String> pplist;
+	
+	@Value("#{tenv.oneStr}")
+	private String helloStr;
+	
+	@Test
+	public void tHello() {
+		assertThat(helloStr, equalTo("hello"));
+	}
+	
 	@Test
 	public void tList() {
-		String[] ss = env.getProperty("ppp.aslist", String[].class);
-		assertThat(ss.length, equalTo(3));
+		assertThat(pplist.get(0), equalTo("abc"));
+		assertThat(pplist.get(1), equalTo("ccd"));
+		assertThat(pplist.size(), equalTo(2));
+	}
+	
+	
+	@TestConfiguration
+	public static  class Tcc {
+		
+		@Bean
+		public Tenv tenv() {
+			return new Tenv();
+		}
+		
+	}
+	
+	public static class Tenv {
+		
+		@Autowired
+		private Environment env;
+		
+		private String oneStr = "hello";
+		
+		public List<String> getAslist() {
+			String base = "ppp.aslist";
+			List<String> values = new ArrayList<>();
+			int i = 0;
+			String v;
+			while((v = env.getProperty("ppp.aslist[" + i + "]")) != null ) {
+				values.add(v);
+				i++;
+			}
+			return values;
+		}
+
+		public String getOneStr() {
+			return oneStr;
+		}
+
+		public void setOneStr(String oneStr) {
+			this.oneStr = oneStr;
+		}
+		
 	}
 
 }
