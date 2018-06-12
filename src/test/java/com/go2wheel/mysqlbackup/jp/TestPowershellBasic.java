c22a40990cb232837495b8049b75c7bd69d53003
jianglibo <jianglibo@gmail.com>
2018-06-12 15:54:05 +0800

    powershell test

--- src/test/java/com/go2wheel/mysqlbackup/jp/TestPowershellBasic.java
+++ src/test/java/com/go2wheel/mysqlbackup/jp/TestPowershellBasic.java
@@ -7,14 +7,17 @@ import static org.junit.Assert.assertTrue;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.util.List;
 import java.util.Map;
 
 import org.junit.Test;
 
 import com.go2wheel.mysqlbackup.util.PSUtil;
+import com.go2wheel.mysqlbackup.util.StringUtil;
 import com.go2wheel.mysqlbackup.value.RemoteCommandResult;
 import com.profesorfalken.jpowershell.PowerShell;
+import com.profesorfalken.jpowershell.PowerShellNotAvailableException;
 import com.profesorfalken.jpowershell.PowerShellResponse;
 
 public class TestPowershellBasic {
@@ -26,18 +29,37 @@ public class TestPowershellBasic {
 		assertThat(so.length(), greaterThan(10));
 	}
 
-//	@Test
-//	public void tpsDriver() {
-//		String cmd = "Get-PSDrive | Where-Object name -Match \"^.{1}$\" | Select-Object name, used, free";
-//		PowerShellResponse response = PowerShell.executeSingleCommand(cmd);
-//		String so = response.getCommandOutput();
-//		List<String> ss = StringUtil.splitLines(so);
-//		String line1 = ss.get(0);
-//		String line2 = ss.get(1);
-//		assertTrue(line1.contains("Name") && line1.contains("Used") && line1.contains("Free"));
-//	}
+	@Test(expected = ArrayIndexOutOfBoundsException.class)
+	public void tpsDriver() {
 
-	@Test
+		
+	   PowerShell powerShell = null;
+	   try {
+	       powerShell = PowerShell.openSession();
+	       String cmd = "Get-PSDrive | Where-Object name -Match '^.{1}$' | Select-Object name, used, free";
+	       
+	       PowerShellResponse response = powerShell.executeCommand(cmd);
+	       powerShell.executeScript(new BufferedReader(new StringReader(cmd)));
+	       
+			String so = response.getCommandOutput();
+			List<String> ss = StringUtil.splitLines(so);
+			String line1 = ss.get(0);
+			String line2 = ss.get(1);
+			assertTrue(line1.contains("Name") && line1.contains("Used") && line1.contains("Free"));
+	       
+	       System.out.println("List Processes:" + response.getCommandOutput());
+	       
+	       response = powerShell.executeCommand("Get-WmiObject Win32_BIOS");
+	       
+	       System.out.println("BIOS information:" + response.getCommandOutput());
+	   } catch(PowerShellNotAvailableException ex) {
+	   } finally {
+	       if (powerShell != null)
+	         powerShell.close();
+	   }
+	}
+
+	@Test()
 	public void testProcessBuilder() throws IOException {
 		ProcessBuilder pb = new ProcessBuilder("powershell.exe",
 				"Get-PSDrive | Where-Object name -Match '^.{1}$' | Select-Object name, used, free");
@@ -60,6 +82,57 @@ public class TestPowershellBasic {
 		stderr.close();
 		System.out.println("Done");
 	}
+	
+	@Test
+	public void testProcessBuilderNotClose() throws IOException {
+		ProcessBuilder pb = new ProcessBuilder("powershell.exe", "-Command", "{Get-PSDrive | Where-Object name -Match '^.{1}$' | Select-Object name, used, free}") ;
+		String cmd = "";
+		Process powerShellProcess = pb.start();
+
+		String line;
+		System.out.println("Standard Output:");
+		BufferedReader stdout = new BufferedReader(new InputStreamReader(powerShellProcess.getInputStream()));
+		while ((line = stdout.readLine()) != null) {
+			System.out.println(line);
+		}
+		stdout.close();
+		System.out.println("Standard Error:");
+		BufferedReader stderr = new BufferedReader(new InputStreamReader(powerShellProcess.getErrorStream()));
+		while ((line = stderr.readLine()) != null) {
+			System.out.println(line);
+		}
+		stderr.close();
+		System.out.println("Done");
+	}
+	
+//	@Test
+//	public void testProcessBuilderInteractive() throws IOException {
+//		ProcessBuilder pb = new ProcessBuilder("powershell.exe","-NoExit", "-InputFormat", "Text",  "-Command", "-") ;
+//		String cmd = "Get-PSDrive | Where-Object name -Match '^.{1}$' | Select-Object name, used, free";
+//		Process powerShellProcess = pb.start();
+//
+//		// Getting the results
+//		PrintWriter pw = new PrintWriter(powerShellProcess.getOutputStream());
+////		.close();
+//		String line;
+//		pw.println(cmd);
+//		System.out.println("Standard Output:");
+//
+//		BufferedReader stdout = new BufferedReader(new InputStreamReader(powerShellProcess.getInputStream()));
+////		pw.println();
+//		while ((line = stdout.readLine()) != null) {
+//			System.out.println(line);
+//		}
+//		stdout.close();
+//		System.out.println("Standard Error:");
+//		BufferedReader stderr = new BufferedReader(new InputStreamReader(powerShellProcess.getErrorStream()));
+//		while ((line = stderr.readLine()) != null) {
+//			System.out.println(line);
+//		}
+//		stderr.close();
+////		pw.println(cmd);
+//		System.out.println("Done");
+//	}
 
 	@Test
 	public void testPsDriverRawJava() throws IOException {
