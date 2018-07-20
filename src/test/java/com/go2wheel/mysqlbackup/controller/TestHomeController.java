b6b722b08ec17a865a77928cd90a2fa000adb795
libo <jianglibo@gmail.com>
2018-07-20 08:25:28 +0800

    minor change.

--- src/test/java/com/go2wheel/mysqlbackup/controller/TestHomeController.java
+++ src/test/java/com/go2wheel/mysqlbackup/controller/TestHomeController.java
@@ -4,31 +4,25 @@ import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.xpath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
 
 import com.go2wheel.mysqlbackup.SpringBaseTWithWeb;
 
 public class TestHomeController extends SpringBaseTWithWeb {
 
-	@Autowired
-	private MockMvc mvc;
-
 	@Test
 	public void testModelAttribute() throws Exception {
-		this.mvc.perform(get("/").accept(MediaType.TEXT_PLAIN)).andExpect(status().isOk())
+		mockMvc.perform(get("/").accept(MediaType.TEXT_PLAIN)).andExpect(status().isOk())
 				.andExpect(content()
 						.string(containsString("class=\"pure-menu-item menu-item-divided pure-menu-selected\"")))
 				.andExpect(content().string(not(containsString("<header>"))))
 				.andExpect(content().string(containsString("Index Page")));
 //				.andExpect(xpath("//ul[@class='pure-menu-list']").exists());
 		
-		this.mvc.perform(get("/app/settings").accept(MediaType.TEXT_PLAIN)).andExpect(status().isOk())
+		mockMvc.perform(get("/app/settings").accept(MediaType.TEXT_PLAIN)).andExpect(status().isOk())
 				.andExpect(content().string(not(containsString("<header>"))))
 				.andExpect(content().string(containsString("Settings")));
 	}
