5867af709c2c42c1ee986cf9c7aedaad48462c58
libo <jianglibo@gmail.com>
2018-07-28 10:38:05 +0800

    begin restore steps.

--- src/main/java/com/go2wheel/mysqlbackup/controller/PlayBacksController.java
+++ src/main/java/com/go2wheel/mysqlbackup/controller/PlayBacksController.java
@@ -2,6 +2,7 @@ package com.go2wheel.mysqlbackup.controller;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpServletRequest;
@@ -12,13 +13,16 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.go2wheel.mysqlbackup.model.PlayBack;
 import com.go2wheel.mysqlbackup.model.Server;
 import com.go2wheel.mysqlbackup.propertyeditor.ListStringToLinesEditor;
 import com.go2wheel.mysqlbackup.service.PlayBackDbService;
 import com.go2wheel.mysqlbackup.service.ServerDbService;
+import com.google.common.collect.Maps;
 
 
 @Controller
@@ -46,6 +50,25 @@ public class PlayBacksController  extends CRUDController<PlayBack, PlayBackDbSer
 		entityFromDb.setSourceServerId(entityFromForm.getSourceServerId());
 		entityFromDb.setTargetServerId(entityFromForm.getTargetServerId());
 		return true;
+	}
+	
+	@GetMapping("/{playback:\\d+}")
+	String getDetail(@PathVariable PlayBack playback, HttpServletRequest request) {
+		ServletUriComponentsBuilder ucb = ServletUriComponentsBuilder.fromRequest(request);
+		Map<String, Object> map = Maps.newHashMap();
+		map.put("playback", playback.getId());
+		String rurl = null;
+		switch (playback.getPlayWhat()) {
+		case PlayBack.PLAY_BORG:
+			rurl = ucb.replacePath("/app/borgrestore/{playback}").buildAndExpand(map).toUriString();
+			return "redirect:" + rurl;   
+		case PlayBack.PLAY_MYSQL:
+			rurl = ucb.replacePath("/app/mysqlrgrestore/{playback}").buildAndExpand(map).toUriString();
+			return "redirect:" + rurl;   
+		default:
+			break;
+		}
+		return "";
 	}
 	
 	@GetMapping("/create")
